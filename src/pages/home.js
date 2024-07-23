04899ac77b840c2c12c9d0e03c23fd2cbbea9620
Spencer Klessens <klessens92@gmail.com>
2024-07-23 14:41:34 -0500

    first test in hope pages folder wiht home.js and edited app.js get updated

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import { Container } from "reactstrap";
+
+const HomePage = () => {
+    return (
+        <Container>
+            <h1>Welcome to my video game collection home page</h1>
+        </Container>
+    )
+};
+
+export default HomePage;
