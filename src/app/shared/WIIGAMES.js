e0cb0cc4958d23f6110307a71b7bb452875ce0d5
Spencer Klessens <klessens92@gmail.com>
2024-09-18 15:48:17 -0500

    new image for all own nes, snes, n64, and gamecube games added.  Wii images edited and added into WIIGAMES.js

--- src/app/shared/WIIGAMES.js
+++ src/app/shared/WIIGAMES.js
@@ -1,46 +1,83 @@
-import nintendoLogo from "../assets/images/Nintendo/wii/wii.png";
+import a from "../assets/images/Nintendo/wii/nintendo_wii.wiisports_US.16258572021403694011.jpg"
+import b from "../assets/images/Nintendo/wii/wii_donkeykongcountryreturns.jpg"
+import c from "../assets/images/Nintendo/wii/wii_metroidprime3corruption.jpg"
+import d from "../assets/images/Nintendo/wii/wii_supermariogalaxy.jpg"
+import e from "../assets/images/Nintendo/wii/wii_supermariogalaxy2.jpg"
+import f from "../assets/images/Nintendo/wii/wii_supersmashbrosbrawl.jpg"
+import g from "../assets/images/Nintendo/wii/wii_thelegendofzeldaskywardsword.jpg"
+import h from "../assets/images/Nintendo/wii/wii_thelegendofzeldatwilightprincess.jpg"
+import i from "../assets/images/Nintendo/wii/wii_wii-sports-resort.jpg"
+import j from "../assets/images/Nintendo/wii/wii_wiiplay.jpg"
 
 export const WII = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
+        name: 'Wii Sports',
+        image: a,
         genre: 'platformer',
         discription: 'Ass'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
+        name: 'Donkey Kong Country Returns',
+        image: b,
         genre: 'shooter',
         discription: 'Ballz'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
+        name: 'Metroid Prime 3: Corruption',
+        image: c,
         genre: 'rpg',
         discription: 'Clams'
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
+        name: 'Super Mario Galaxy',
+        image: d,
+        genre: '3D Platformer',
         discription: 'Dislexiua'
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
+        name: 'Super Mario Galaxy 2',
+        image: e,
+        genre: '3D Platformer',
         discription: 'E is for pants'
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
+        name: 'Super SMash Bros Brawl',
+        image: f,
+        genre: 'fighting',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 6,
+        name: 'The Legend of Zelda: Skyward Sword',
+        image: g,
+        genre: 'Action Adventure',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 7,
+        name: 'The Legend of Zelda: Twilight Princess',
+        image: h,
+        genre: 'Action Adventure',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 8,
+        name: 'Wii Play',
+        image: i,
+        genre: 'fighting',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 9,
+        name: 'Wii Sports Resort',
+        image: j,
+        genre: 'fighting',
         discription: 'GRRRRREEEENNNNN'
     },
 ];
