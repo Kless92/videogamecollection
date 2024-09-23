dcc3fe78a4739514be44df929402661eed03a98d
Spencer Klessens <klessens92@gmail.com>
2024-09-23 13:49:59 -0500

    images for gamecube is edited and added to GAMECUBEGAMES.js

--- src/app/shared/GAMECUBEGAMES.js
+++ src/app/shared/GAMECUBEGAMES.js
@@ -1,46 +1,124 @@
 import nintendoLogo from "../assets/images/Nintendo/gamecube/gamecube.jpg";
+import ffCrystalChronicles from "../assets/images/Nintendo/gamecube/gc_finalfantasycrystalchronicles.jpg"
+import Zelda4SwordsAdv from "../assets/images/Nintendo/gamecube/gc_legendofzeldafourswordsadventures.jpg"
+import WindWaker from "../assets/images/Nintendo/gamecube/gc_legendofzeldathewindwaker_us.jpg"
+import MarioKartDouble from "../assets/images/Nintendo/gamecube/gc_mariokartdoubledash.jpg"
+import MarioParty7 from "../assets/images/Nintendo/gamecube/gc_marioparty7.jpg"
+import MetroidPrime from "../assets/images/Nintendo/gamecube/gc_metroidprime.jpg"
+import MetroidPrime2 from "../assets/images/Nintendo/gamecube/gc_metroidprime2echoes.jpg"
+import Pikmin from "../assets/images/Nintendo/gamecube/gc_pikmin.jpg"
+import RE1 from "../assets/images/Nintendo/gamecube/gc_residentevil.jpg"
+import RE0 from "../assets/images/Nintendo/gamecube/gc_residentevil_zero.jpg"
+import RE2 from "../assets/images/Nintendo/gamecube/gc_residentevil2.jpg"
+import RE4 from "../assets/images/Nintendo/gamecube/gc_residentevil4.jpg"
+import SoulCalibur2 from "../assets/images/Nintendo/gamecube/gc_soulcalibur2_eu.jpg"
+import SunShine from "../assets/images/Nintendo/gamecube/gc_supermariosunshine.jpg"
+import SSBmelee from "../assets/images/Nintendo/gamecube/gc_supersmashbrothersmelee.jpg"
 
 export const GAMECUBE = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: 'Final Fantasy: Crystal Chroicals',
+        image: ffCrystalChronicles,
+        genre: 'Action RPG, JRPG',
+        discription: 'Travel the world by your self, or with friends, collection myrrh to maintain the village crystal.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'The Legend of Zelda: Four Swords Adventures',
+        image: Zelda4SwordsAdv,
+        genre: 'Action Adventure',
+        discription: 'Save Hylrule with the 4 sword.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'      
+        name: 'The Legend of Zelda: The Wind Waker',
+        image: WindWaker,
+        genre: 'Action Adventure',
+        discription: 'Sail the high seas and uncover the secrets below.'
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'        
+        name: 'Mario Kart: Double Dash',
+        image: MarioKartDouble,
+        genre: 'Raceing',
+        discription: 'Race 16 differnt cources aginst 9 drivers; each with a co-pilot.'
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Mario Party 7',
+        image: MarioParty7,
+        genre: 'Party Video Games',
+        discription: 'The seventh installment of the Mario Party series.'
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'fff'        
+        name: 'Metroid Prime',
+        image: MetroidPrime,
+        genre: 'FPS, Platformer',
+        discription: 'The first First Person Shooter Metriod game; explore a new world while uncover power ups and secrets.'
     },
+    {
+        id: 6,
+        name: 'Metroid Prime 2',
+        image: MetroidPrime2,
+        genre: 'FPS, Platformer',
+        discription: 'The sequal the Metroid prime; uncovering new items though exploration.'
+    },
+    {
+        id: 7,
+        name: 'Pikmin',
+        image: Pikmin,
+        genre: 'Real-time Strategy, Action Adventure',
+        discription: 'Collect pieces of your ship from your little friends.'
+    },
+    {
+        id: 8,
+        name: 'Resident Evil',
+        image: RE1,
+        genre: 'Survival Horror',
+        discription: 'Remake of the PS1 hit. Returning to the Spencers Mansion and face aginst zombies and mosters.'
+    },
+    {
+        id: 9,
+        name: 'Resident Evil: 0',
+        image: RE0,
+        genre: 'Survival Horror',
+        discription: 'Play the event the transpire before the events in Resident Evil.'
+    },
+    {
+        id: 10,
+        name: 'Resident Evil: 2',
+        image: RE2,
+        genre: 'Survival Horror',
+        discription: 'Port of the PS1 version. Play Leon or Clear in the own stories surviving Raccon Citys infection.'
+    },
+    {
+        id: 11,
+        name: 'Resident Evil: 4',
+        image: RE4,
+        genre: 'Survival Horror',
+        discription: 'Leon Kennedy travles to Europe to save the President daughter for a infectious cult.'
+    },
+    {
+        id: 12,
+        name: 'Soul Calibur 2',
+        image: SoulCalibur2,
+        genre: 'Fighting',
+        discription: 'One on one fights with character fromt the previous title, with Link from Legend of Zelda as an exclusive playable character.'
+    },
+    {
+        id: 13,
+        name: 'Super Mario: Sun Shine',
+        image: SunShine,
+        genre: 'Platformer',
+        discription: 'Marios newest adventure in Isle Delfino'
+    },
+    {
+        id: 14,
+        name: 'Super Smash Bros: Melee',
+        image: SSBmelee,
+        genre: 'Fighting',
+        discription: 'Everyones favorte Nintendo characters returnes with new challengers and new stages and modes.'
+    }
 ];
