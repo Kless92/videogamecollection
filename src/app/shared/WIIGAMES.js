9b3ebbfac301753abcec3e462ba1df46909b9a84
Spencer Klessens <klessens92@gmail.com>
2024-09-24 19:07:02 -0500

    update WIIGAMES and WIIUGAMES.

--- src/app/shared/WIIGAMES.js
+++ src/app/shared/WIIGAMES.js
@@ -14,70 +14,70 @@ export const WII = [
         id: 0,
         name: 'Wii Sports',
         image: a,
-        genre: 'platformer',
-        discription: 'Ass'
+        genre: 'Sports',
+        discription: 'Came with most Wii System containing five games: Tennis, Baseball, Bowling, Golf and Boxing.'
     },
     {
         id: 1,
         name: 'Donkey Kong Country Returns',
         image: b,
-        genre: 'shooter',
-        discription: 'Ballz'
+        genre: 'Platformer',
+        discription: 'Donkey Kong returns in the long waitied fourth installment of the DK country series.'
     },
     {
         id: 2,
         name: 'Metroid Prime 3: Corruption',
         image: c,
-        genre: 'rpg',
-        discription: 'Clams'
+        genre: 'FPS, Platformer',
+        discription: 'The third Metroid Prime game were Samus must stop the corruption.'
     },
     {
         id: 3,
         name: 'Super Mario Galaxy',
         image: d,
         genre: '3D Platformer',
-        discription: 'Dislexiua'
+        discription: 'Travel to different worlds in Mario outer space adventures.'
     },
     {
         id: 4,
         name: 'Super Mario Galaxy 2',
         image: e,
         genre: '3D Platformer',
-        discription: 'E is for pants'
+        discription: 'Mario returns to outer space with some friends.'
     },
     {
         id: 5,
-        name: 'Super SMash Bros Brawl',
+        name: 'Super Smash Bros Brawl',
         image: f,
         genre: 'fighting',
-        discription: 'GRRRRREEEENNNNN'
+        discription: 'The third Super Smash Bros title now on the wii with a new story mode and online multiplayer.'
     },
     {
         id: 6,
         name: 'The Legend of Zelda: Skyward Sword',
         image: g,
         genre: 'Action Adventure',
-        discription: 'GRRRRREEEENNNNN'
+        discription: 'Travel across the land and sky in links newest adventure to find Zelda.'
     },
     {
         id: 7,
         name: 'The Legend of Zelda: Twilight Princess',
         image: h,
         genre: 'Action Adventure',
-        discription: 'GRRRRREEEENNNNN'
+        discription: 'Enter the realm of Twilight with Link as he saved Hylrule once again.'
     },
     {
         id: 8,
         name: 'Wii Play',
         image: i,
-        genre: 'fighting',
-        discription: 'GRRRRREEEENNNNN'
+        genre: 'Party Games.',
+        discription: 'Play nine party games with friends.'
     },
     {
         id: 9,
         name: 'Wii Sports Resort',
         image: j,
         genre: 'fighting',
-        discription: 'GRRRRREEEENNNNN'
+        discription: 'The sequel to wii sports now on a island resort with new events with wii motion plus.'
     },
 ];
--- src/app/shared/WIIUGAMES.js
+++ src/app/shared/WIIUGAMES.js
@@ -12,56 +12,56 @@ export const WIIU = [
         id: 0,
         name: 'The Legend of Zelda: Breath of the Wild',
         image: a,
-        genre: 'platformer',
-        discription: 'Ass'
+        genre: 'Open World, Action-Adventure',
+        discription: 'Links newest "Massive" open world were he must save Hylrule after one hundred year sleep.'
     },
     {
         id: 1,
         name: 'NES Remix',
         image: b,
-        genre: 'shooter',
-        discription: 'Ballz'
+        genre: 'Action, Arcade',
+        discription: 'Replay classic NES game with new rules and challenges.'
     },
     {
         id: 2,
         name: 'Nintendo Land',
         image: c,
-        genre: 'rpg',
-        discription: 'Clams'
+        genre: 'Party Games',
+        discription: 'Enter the wrold and Nintendo Land, trying out the new GamePad in diffrent style of game play.'
     },
     {
         id: 3,
         name: 'Pikmin 3',
         image: d,
-        genre: 'puzzle',
-        discription: 'Dislexiua'
+        genre: 'Real-Time Strategy',
+        discription: 'Return PNF-404 as three crew member must find food for the world and there going to need help.'
     },
     {
         id: 4,
         name: 'Super Mario 3D world',
         image: e,
-        genre: 'platformer',
-        discription: 'E is for pants'
+        genre: 'Platformer',
+        discription: 'Mario must stop Bowser again but this time he can bring up to four friends.'
     },
     {
         id: 5,
         name: 'Super Mario Maker',
         image: f,
-        genre: 'rpg',
-        discription: 'GRRRRREEEENNNNN'
+        genre: 'Platformer',
+        discription: 'Make your very own Mario level and share it with the world or play everyone elses level or both.'
     },
     {
         id: 6,
         name: 'Super Smash Bros. for WiiU',
         image: g,
-        genre: 'rpg',
-        discription: 'GRRRRREEEENNNNN'
+        genre: 'Fighter',
+        discription: 'For Wii U or 3DS, enter the areans with new and returning characters in the fourth installment.'
     },
     {
         id: 7,
         name: 'The Legend of Zelda: The Wind Waker HD',
         image: h,
-        genre: 'rpg',
-        discription: 'GRRRRREEEENNNNN'
+        genre: 'Action-Adventure',
+        discription: 'The HD reamke of the hit gamecube game reutrn the set sail.'
     },
 ];
