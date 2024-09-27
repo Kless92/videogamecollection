6d3b30cae696a451dfe3a43b1bba46b86f81013f
Spencer Klessens <klessens92@gmail.com>
2024-09-27 17:12:42 -0500

    all currently own games added to N64GAMES.

--- src/app/shared/N64GAMES.js
+++ src/app/shared/N64GAMES.js
@@ -7,7 +7,7 @@ import goldenEye from "../assets/images/Nintendo/n64/n64_goldeneye.jpg";
 import kirby64 from "../assets/images/Nintendo/n64/n64_kirby64thecrystalshards.jpg";
 import marioParty2 from "../assets/images/Nintendo/n64/n64_marioparty2.jpg";
 import perfectDark from "../assets/images/Nintendo/n64/n64_perfectdark.jpg";
-import pokmonSnap from "../assets/images/Nintendo/n64/n64_pokemonsnap.jpg";
+import pokemonSnap from "../assets/images/Nintendo/n64/n64_pokemonsnap.jpg";
 import pokemonStadium from "../assets/images/Nintendo/n64/n64_pokemonstadium.jpg";
 import rayman2 from "../assets/images/Nintendo/n64/n64_rayman2alt.jpg";
 import rugratsScavengerHunt from "../assets/images/Nintendo/n64/n64_rugratsscavengerhunt.jpg";
@@ -70,11 +70,95 @@ export const N64 = [
         discription: 'Collect crystal shards and save many worlds form a new threat.'
     },
     {
-        id: 6,
+        id: 7,
         name: 'Mario Party 2',
         image: marioParty2,
         genre: 'Party Game',
         discription: 'Play aginst friends in 6 theme boards in 62 diffrent mini games.'
     },
+    {
+        id: 8,
+        name: 'Perfect Dark',
+        image: perfectDark,
+        genre: 'First Person Shooter',
+        discription: 'A secret againt fight agist aliens'
+    },
+    {
+        id: 9,
+        name: 'Pokemon Snap',
+        image: pokemonSnap,
+        genre: 'Raile Shooter',
+        discription: 'Take pictures of Pokemon in diffrent environments.'
+    },
+    {
+        id: 10,
+        name: 'Pokemon Stadium',
+        image: pokemonStadium,
+        genre: 'Turn-Based Strategy',
+        discription: 'Battle in this 3D version of Pokemon.'
+    },
+    {
+        id: 11,
+        name: 'Rayman 2',
+        image: rayman2,
+        genre: 'Platformer',
+        discription: 'Rayman returns to save his home from pirates.'
+    },
+    {
+        id: 12,
+        name: 'Rugrats Scavenger Hunt',
+        image: rugratsScavengerHunt,
+        genre: 'Party Game',
+        discription: 'Play three board games with Nickaldeon famous babies.'
+    },
+    {
+        id: 13,
+        name: 'Star Fox 64',
+        image: starFox64,
+        genre: 'Raile Shooter',
+        discription: 'Fox returnes in this 3D instalment.'
+    },
+    {
+        id: 14,
+        name: 'Shadows of teh Empire',
+        image: shadowsOfTheEmpire,
+        genre: 'Third Person Shooter',
+        discription: 'Play as Dash Rendar in this Star Wars extended universe.'
+    },
+    {
+        id: 15,
+        name: 'Super Mario 64',
+        image: superMario64,
+        genre: 'Platformer',
+        discription: 'Mario\'s first 3D adventure.'
+    },
+    {
+        id: 16,
+        name: 'Super Smash Bros',
+        image: superSmashBros,
+        genre: 'Fighting Game',
+        discription: 'Twelve iconic Nintendo character duke it out.'
+    },
+    {
+        id: 17,
+        name: 'The Legend of Zelda: Majoras Mask',
+        image: majorasMask,
+        genre: 'Action-Adventure',
+        discription: 'In the sequel of Ocarina of Time, Link travles to Termina to save the world in three days.'
+    },
+    {
+        id: 18,
+        name: 'The Legend of Zelda: Ocarina of Time',
+        image: ocarinaOfTime,
+        genre: 'Action-Adventure',
+        discription: 'The first 3D Zelda game, Link must save Hylre in the present and future.'
+    },
+    {
+        id: 19,
+        name: 'Yoshis Story',
+        image: yoshisStory,
+        genre: 'Platformer',
+        discription: 'The yoshis must stop Baby Bowser and save there home island.'
+    }
     
 ];
