48cab1d2a9379d6f65b61b545d28f5a53b3fd623
Spencer Klessens <klessens92@gmail.com>
2024-09-27 13:19:36 -0500

    all nintendo 64 game images are rotatted.

--- src/app/shared/N64GAMES.js
+++ src/app/shared/N64GAMES.js
@@ -1,46 +1,72 @@
-import nintendoLogo from "../assets/images/Nintendo/n64/n64.png";
+import banjoKazooie from "../assets/images/Nintendo/n64/n64_banjokazooie.jpg";
+import banjoTooie from "../assets/images/Nintendo/n64/n64_banjotooie.jpg";
+import diddyKongRacing from "../assets/images/Nintendo/n64/n64_diddykongracing.jpg";
+import donkeyKong64 from "../assets/images/Nintendo/n64/n64_donkeykong64.jpg";
+import glover from "../assets/images/Nintendo/n64/n64_glover.jpg";
+import goldenEye from "../assets/images/Nintendo/n64/n64_goldeneye.jpg";
+import kirby64 from "../assets/images/Nintendo/n64/n64_kirby64thecrystalshards.jpg";
+import marioParty2 from "../assets/images/Nintendo/n64/n64_marioparty2.jpg";
+import perfectDark from "../assets/images/Nintendo/n64/n64_perfectdark.jpg";
+import pokmonSnap from "../assets/images/Nintendo/n64/n64_pokemonsnap.jpg";
+import pokemonStadium from "../assets/images/Nintendo/n64/n64_pokemonstadium.jpg";
+import rayman2 from "../assets/images/Nintendo/n64/n64_rayman2alt.jpg";
+import rugratsScavengerHunt from "../assets/images/Nintendo/n64/n64_rugratsscavengerhunt.jpg";
+import starFox64 from "../assets/images/Nintendo/n64/n64_starfox64.jpg";
+import shadowsOfTheEmpire from "../assets/images/Nintendo/n64/n64_starwarsshadowsoftheempire.jpg";
+import superMario64 from "../assets/images/Nintendo/n64/n64_supermario64.jpg";
+import superSmashBros from "../assets/images/Nintendo/n64/n64_supersmashbros.jpg";
+import majorasMask from "../assets/images/Nintendo/n64/n64_thelegendofzeldamajorasmask.jpg";
+import ocarinaOfTime from "../assets/images/Nintendo/n64/n64_thelegendofzeldaocarinaoftime.jpg";
+import yoshisStory from "../assets/images/Nintendo/n64/n64_yoshisstory.jpg";
 
 export const N64 = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'Ass'
+        name: 'Banjo-Kazooie',
+        image: banjoKazooie,
+        genre: 'Platformer',
+        discription: 'Enter Gruntilda\'s lair to save Tooty.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'Ballz'
+        name: 'Banjo-Tooie',
+        image: banjoTooie,
+        genre: 'Platformer',
+        discription: 'Go after Gruntilda in Banjo\'s sequel.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'Clams'
+        name: 'Diddy Kong Racing',
+        image: diddyKongRacing,
+        genre: 'Racing',
+        discription: 'Race with Diddy Kong and his friends aginst Wizping.'
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'Dislexiua'
+        name: 'Donkey Kong 64',
+        image: donkeyKong64,
+        genre: 'Platformer',
+        discription: 'Donkey Kong 3D adventure aginst King K. Rule.'
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
+        name: 'Glover',
+        image: glover,
+        genre: 'Platformer',
         discription: 'E is for pants'
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'GRRRRREEEENNNNN'
+        name: 'Golden Eye 007',
+        image: goldenEye,
+        genre: 'First Person Shooter',
+        discription: 'Jame Bond goes aginst impossible odds to save the world'
+    },
+    {
+        id: 6,
+        name: 'Kirby 64 The Crystal Shards',
+        image: kirby64,
+        genre: 'Platformer',
+        discription: 'Collect crystal shards and save many worlds form a new threat.'
     },
 ];
