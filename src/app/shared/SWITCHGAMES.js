02e74c30b99a5aabc16c74d2fd71330243ad56ed
Spencer Klessens <klessens92@gmail.com>
2024-09-30 14:46:42 -0500

    switch image added and info edited in SWITCHGAMES.js

--- src/app/shared/SWITCHGAMES.js
+++ src/app/shared/SWITCHGAMES.js
@@ -1,46 +1,99 @@
-import nintendoLogo from "../assets/images/Nintendo/switch/switch.png";
+import crashTrilogy from "../assets/images/Nintendo/switch/nintendo_switch.crashbandicootnsanetrilogy_US.16416757931781361642.jpg";
+import ffxii from "../assets/images/Nintendo/switch/nintendo_switch.finalfantasyxiithezodiacage.jpg";
+import zeldaBOTW from "../assets/images/Nintendo/switch/nintendo_switch.legendofzeldabreathofthewild.jpg";
+import zeldaTOTK from "../assets/images/Nintendo/switch/nintendo_switch.legendofzeldatearsofthekingdomthe_US.16888410521241815663.jpg";
+import luigisMansion3 from "../assets/images/Nintendo/switch/nintendo_switch.luigismansion3_US.1637706730907733872.jpg";
+import megaMan11 from "../assets/images/Nintendo/switch/nintendo_switch.megaman11_.1689671181200428452.jpg";
+import metroidDread from "../assets/images/Nintendo/switch//nintendo_switch.metroiddread_US.164041622196300850.jpg";
+import newSuperMarioBrosDexluxe from "../assets/images/Nintendo/switch/nintendo_switch.newsupermariobrosudeluxe_US.16417698001364096373.jpg";
+import splatoon2 from "../assets/images/Nintendo/switch/nintendo_switch.splatoon2_US.16316825121648943040.jpg";
+import superMario3DAllStars from "../assets/images/Nintendo/switch/nintendo_switch.supermario3dallstars_US.1641779059599328592.jpg";
+import superMarioMaker2 from "../assets/images/Nintendo/switch/nintendo_switch.supermariomaker2_US.16417791361871780190.jpg";
+import superSmashBrosUltimate from "../assets/images/Nintendo/switch/nintendo_switch.supersmashbrosultimate_US.16417793672031643572.jpg";
 
 export const SWITCH = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
+        name: 'Crash Bandicoot N. Sane Trilogy',
+        image: crashTrilogy,
         genre: 'platformer',
-        discription: 'Ass'
+        discription: 'HD remake of Naughty Dog PS1 trilogy: Crash Bandicoot.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'Ballz'
+        name: 'Final Fantasy XII: The Zodiac Age ',
+        image: ffxii,
+        genre: 'RPG',
+        discription: 'Remake of the PS2 Hit, introducting new class system.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'Clams'
+        name: 'The Legend of Zelda: Breath of the Wild',
+        image: zeldaBOTW,
+        genre: 'Open World, Action-Adventure',
+        discription: 'Links newest "Massive" open world were he must save Hylrule after one hundred year sleep.'
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'Dislexiua'
+        name: 'The Legend of Zelda: Tears of the Kingdom',
+        image: zeldaTOTK,
+        genre: 'Open World, Action-Adventure',
+        discription: 'Sequel to Breath of the Wild, Link my find Princess Zelda after she disapared and save Hyrule after a event called upheavel.'
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'E is for pants'
+        name: 'Luigis Mansion 3',
+        image: luigisMansion3,
+        genre: 'Action-Adventure',
+        discription: 'Luigi must save Mario, Peach and friends against ghost in a haunted hotel.'
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'GRRRRREEEENNNNN'
+        name: 'Mega Man 11',
+        image: megaMan11,
+        genre: 'Shooter',
+        discription: 'Mega Man return to stop Dr. Wily using the new Double Gear System.'
+    },
+    {
+        id: 7,
+        name: 'Metroid Dread',
+        image: metroidDread,
+        genre: 'Action-Adventure',
+        discription: 'Samus is sent to ZDR to investigate the disappearance of the EMMI.'
+    },
+    {
+        id: 8,
+        name: 'New Super Mario Bros. U Dexluxe',
+        image: newSuperMarioBrosDexluxe,
+        genre: '2.5D Platformer',
+        discription: 'Mario and friends return as a port of this WiiU title.'
+    },
+    {
+        id: 9,
+        name: 'Splatoon 2',
+        image: splatoon2,
+        genre: 'Thrid Person Shooter',
+        discription: 'The sequle to the WiiU mutiplayer shooter is now on switch with a single player campgin and more.'
+    },
+    {
+        id: 10,
+        name: 'Super Mario 3D All Stars',
+        image: superMario3DAllStars,
+        genre: '3D Platformer',
+        discription: 'Super Mario 64, Sunshine and Galaxy is ported for a limited time to the switch.'
+    },
+    {
+        id: 11,
+        name: 'Super Mario Maker 2',
+        image: superMarioMaker2,
+        genre: 'Platformer',
+        discription: 'Sequle to Mario Maker now on swtich with new features for more unique levels to make.'
+    },
+    {
+        id: 12,
+        name: 'Super Smash Bros Ultimate',
+        image: superSmashBrosUltimate,
+        genre: 'Fighter',
+        discription: 'Fifth in the series includes all fighters; past, prestent and new; come to fight.'
     },
 ];
