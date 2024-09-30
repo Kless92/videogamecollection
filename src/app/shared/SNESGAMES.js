7cbf3667d47cf0436d300bb801fd6e1afda9c3c1
Spencer Klessens <klessens92@gmail.com>
2024-09-30 17:39:24 -0500

    SNESGAMES.js is edited witha  few items added.

--- src/app/shared/SNESGAMES.js
+++ src/app/shared/SNESGAMES.js
@@ -1,46 +1,117 @@
 import nintendoLogo from "../assets/images/Nintendo/snes/snes.jpg";
+import a from "../assets/images/Nintendo/snes/snes_aladdin.jpg";
+import b from "../assets/images/Nintendo/snes/snes_donkeykongcountry.jpg";
+import c from "../assets/images/Nintendo/snes/snes_dkc2final.jpg";
+import d from "../assets/images/Nintendo/snes/snes_gooftroop.jpg";
+import e from "../assets/images/Nintendo/snes/snes_gradius3.jpg";
+import f from "../assets/images/Nintendo/snes/snes_megamanx.jpg";
+import g from "../assets/images/Nintendo/snes/snes_streetfighteriiturbo.jpg";
+import h from "../assets/images/Nintendo/snes/snes_supergameboy_US.1634097593146777238.jpg";
+import i from "../assets/images/Nintendo/snes/snes_supermarioallstars_US.16319127801837786814.jpg";
+import j from "../assets/images/Nintendo/snes/snes_supermarioworld.jpg";
+import k from "../assets/images/Nintendo/snes/snes_supermetroid.jpg";
+import l from "../assets/images/Nintendo/snes/snes_superreturnofthejedi.jpg";
+import m from "../assets/images/Nintendo/snes/snes_thelegendofzeldaalinktothepast.jpg";
+import n from "../assets/images/Nintendo/snes/snes_zombiesatemyneighbors.jpg";
+
 
 export const SNES = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'Ass'
+        name: 'Aladdin',
+        image: a,
+        genre: 'Platformer',
+        discription: 'The 1992 movie tie-in now on Super Nintendo'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'Ballz'
+        name: 'Donkey Kong Country',
+        image: b,
+        genre: 'Platformer',
+        discription: 'Recover Donkey Kong\'s banannas fro King K. Rool.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'Clams'
+        name: 'Donkey Kong Country 2: Diddies Kong Quest',
+        image: c,
+        genre: 'Platformer',
+        discription: 'Diddy and Dixie off to save Donky Kong form Kaption K. Rool.'
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'Dislexiua'
+        name: 'Goof Troop',
+        image: d,
+        genre: 'Action-Adventure',
+        discription: 'Goofy and Max are on a island filled with pirates and they must save Pete and P.J.'
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'E is for pants'
+        name: 'Gradius 3',
+        image: e,
+        genre: 'Shoot\'em Up',
+        discription: 'Konami space shooter is back with new weapons and worlds and bosses to fight.'
     },
     {
         id: 5,
+        name: 'MegaMan X',
+        image: f,
+        genre: 'Run\'n Gun',
+        discription: 'A new hero name X must save humanity from Sigma and his Mavericks.'
+    },
+    {
+        id: 6,
+        name: 'Street Fighter II: Turbo',
+        image: g,
+        genre: 'Fighter',
+        discription: 'The fourth edition to be released with new advancements.'
+    },
+    {
+        id: 7,
         name: 'GameSix',
         image: nintendoLogo,
         genre: 'rpg',
         discription: 'GRRRRREEEENNNNN'
     },
+    {
+        id: 8,
+        name: 'GameSix',
+        image: nintendoLogo,
+        genre: 'rpg',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 9,
+        name: 'GameSix',
+        image: nintendoLogo,
+        genre: 'rpg',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 10,
+        name: 'GameSix',
+        image: nintendoLogo,
+        genre: 'rpg',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 11,
+        name: 'GameSix',
+        image: nintendoLogo,
+        genre: 'rpg',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 12,
+        name: 'GameSix',
+        image: nintendoLogo,
+        genre: 'rpg',
+        discription: 'GRRRRREEEENNNNN'
+    },
+    {
+        id: 13,
+        name: 'GameSix',
+        image: nintendoLogo,
+        genre: 'rpg',
+        discription: 'GRRRRREEEENNNNN'
+    }
 ];
