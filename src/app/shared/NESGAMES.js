4e1a73dd0a4e6e75bfa27cf4690ca3e1dfe97628
Spencer Klessens <klessens92@gmail.com>
2024-10-04 13:57:56 -0500

    all own nes games are added in NESGAMES.js

--- src/app/shared/NESGAMES.js
+++ src/app/shared/NESGAMES.js
@@ -2,33 +2,35 @@ import a from "../assets/images/Nintendo/nes/nes_bioniccommando_US.1707718798571
 import b from "../assets/images/Nintendo/nes/nes_blastermaster.jpg"
 import c from "../assets/images/Nintendo/nes/nes_castlevania.jpg"
 import d from "../assets/images/Nintendo/nes/nes_castlevania2.jpg"
-import e from "../assets/images/Nintendo/nes/nes_contra.jpgg"
-import f from "../assets/images/Nintendo/nes/nes_doubledragon_2.jpg"
-import g from "../assets/images/Nintendo/nes/nes_dragonwarriror.jpg"
-import h from "../assets/images/Nintendo/nes/nes_drmario.jpg"
-import i from "../assets/images/Nintendo/nes/nes_duckhunt_US.1699252929819828341.jpg"
-import j from "../assets/images/Nintendo/nes/nes_excitebike.jpg"
-import k from "../assets/images/Nintendo/nes/nes_gauntlet2.jpg"
-import l from "../assets/images/Nintendo/nes/nes_hogansalley_eu.jpg"
-import m from "../assets/images/Nintendo/nes/nes_icehockey.jpg"
-import n from "../assets/images/Nintendo/nes/nes_kirbysadventure.jpg"
-import o from "../assets/images/Nintendo/nes/nes_legendofzelda_3.jpg"
-import p from "../assets/images/Nintendo/nes/nes_legendofzeldaadevntureoflink.jpg"
-import q from "../assets/images/Nintendo/nes/nes_megaman1_5.jpg"
-import r from "../assets/images/Nintendo/nes/nes_megaman2_US.1708147044323255657.jpg"
-import s from "../assets/images/Nintendo/nes/nes_megaman3_US.17081471251917397318.jpg"
-import t from "../assets/images/Nintendo/nes/nes_megaman6_US.17081473071833912795.jpg"
-import u from "../assets/images/Nintendo/nes/nes_metroid.jpg"
-import v from "../assets/images/Nintendo/nes/nes_miketysonspunchout_US.17020118591263188761.jpg"
-import w from "../assets/images/Nintendo/nes/nes_radracer.jpg"
-import x from "../assets/images/Nintendo/nes/nes_superc.jpg"
-import y from "../assets/images/Nintendo/nes/nes_supermariobros_US.16992520461060231782.jpg"
-import z from "../assets/images/Nintendo/nes/nes_supermariobros2.jpg"
-import aa from "../assets/images/Nintendo/nes/nes_supermariobros3_US.170072101915978713.jpg"
-import bb from "../assets/images/Nintendo/nes/nes_tecmosuperbowl.jpg"
-import cc from "../assets/images/Nintendo/nes/nes_teenagemutantninjaturtles_US.1710048272596910426.jpg"
-import dd from "../assets/images/Nintendo/nes/nes_Tetris2.jpg"
-import ee from "../assets/images/Nintendo/nes/nes_world-class-track-meet.jpg"
+import e from "../assets/images/Nintendo/nes/nes_chipndalerescuerangers.jpg"
+import f from "../assets/images/Nintendo/nes/nes_contra.jpg"
+import g from "../assets/images/Nintendo/nes/nes_doubledragon_2.jpg"
+import h from "../assets/images/Nintendo/nes/nes_dragonwarriror.jpg"
+import i from "../assets/images/Nintendo/nes/nes_drmario.jpg"
+import j from "../assets/images/Nintendo/nes/nes_duckhunt_US.1699252929819828341.jpg"
+import k from "../assets/images/Nintendo/nes/nes_excitebike.jpg"
+import l from "../assets/images/Nintendo/nes/nes_gauntlet2.jpg"
+import m from "../assets/images/Nintendo/nes/nes_hogansalley_eu.jpg"
+import n from "../assets/images/Nintendo/nes/nes_icehockey.jpg"
+import o from "../assets/images/Nintendo/nes/nes_kirbysadventure.jpg"
+import p from "../assets/images/Nintendo/nes/nes_legendofzelda_3.jpg"
+import q from "../assets/images/Nintendo/nes/nes_legendofzeldaadevntureoflink.jpg"
+import r from "../assets/images/Nintendo/nes/nes_megaman1_5.jpg"
+import s from "../assets/images/Nintendo/nes/nes_megaman2_US.1708147044323255657.jpg"
+import t from "../assets/images/Nintendo/nes/nes_megaman3_US.17081471251917397318.jpg"
+import u from "../assets/images/Nintendo/nes/nes_megaman6_US.17081473071833912795.jpg"
+import v from "../assets/images/Nintendo/nes/nes_metroid.jpg"
+import w from "../assets/images/Nintendo/nes/nes_miketysonspunchout_US.17020118591263188761.jpg"
+import x from "../assets/images/Nintendo/nes/nes_radracer.jpg"
+import y from "../assets/images/Nintendo/nes/nes_superc.jpg"
+import z from "../assets/images/Nintendo/nes/nes_supermariobros_US.16992520461060231782.jpg"
+import aa from "../assets/images/Nintendo/nes/nes_supermariobros2.jpg"
+import bb from "../assets/images/Nintendo/nes/nes_supermariobros3_US.170072101915978713.jpg"
+import cc from "../assets/images/Nintendo/nes/nes_tecmosuperbowl.jpg"
+import dd from "../assets/images/Nintendo/nes/nes_teenagemutantninjaturtles_US.1710048272596910426.jpg"
+import ee from "../assets/images/Nintendo/nes/nes_tetris2.jpg"
+import ff from "../assets/images/Nintendo/nes/nes_world-class-track-meet.jpg"
+
 
 export const NES = [
     {
@@ -75,65 +77,184 @@ export const NES = [
     },
     {
         id: 6,
-        name: 'Dragon Warriror',
+        name: 'Double Dragon',
         image: g,
-        genre: 'rpg',
-        discription: 'Save the land in this turn base RPG.'        
+        genre: 'Beat\'em Up',
+        discription: 'Billy and Jimmy are off the save there girl.'        
     },
     {
         id: 7,
-        name: 'Duck Hunt',
+        name: 'Dragon Warriror',
         image: h,
+        genre: 'RPG',
+        discription: 'Save the land in this turn base RPG.'        
+    },
+    {
+        id: 8,
+        name: 'Dr. Mario',
+        image: i,
+        genre: 'Puzzle',
+        discription: 'Kill some vireses as Dr. mario in this puzzle classic.'        
+    },
+    {
+        id: 9,
+        name: 'Duck Hunt',
+        image: j,
         genre: 'Light-gun Shooter',
         discription: 'Shoot duck or you dog will mock you.'        
     },
     {
-        id: 8,
+        id: 10,
         name: 'Excite Bike',
-        image: i,
+        image: k,
         genre: 'Racing',
         discription: 'Race aginst your opponents on dirt bikes on many courses.'        
     },
     {
-        id: 9,
+        id: 11,
         name: 'Gauntlet 2',
-        image: i,
+        image: l,
         genre: 'Hack & Slash',
         discription: 'Port of the Arcade, play one of four classes in this dungeon crawler.'        
     },
     {
-        id: 10,
+        id: 12,
         name: 'Hogans Alley',
-        image: i,
+        image: m,
         genre: 'Light-gun Shooter',
         discription: 'Play diffrent course in each round as you shoot your targets.'        
     },
     {
-        id: 11,
+        id: 13,
         name: 'Ice Hockey',
-        image: i,
+        image: n,
         genre: 'Sport',
         discription: 'Play ice hockey on the NES as you play with a friend or the computer.'        
     },
     {
-        id: 12,
+        id: 14,
         name: 'Kirby\'s Adventure',
-        image: i,
-        genre: 'Racing',
+        image: o,
+        genre: 'Platformer',
         discription: 'Kirbys off again to save Dream Land,'        
     },
     {
-        id: 13,
+        id: 15,
         name: 'The Legend of Zelda',
-        image: i,
+        image: p,
         genre: 'Action-Adventure',
         discription: 'Recover the trifoce shard and save Zelda from Ganon'        
     },
     {
-        id: 14,
+        id: 16,
         name: 'Zelda II: The Adventure of Link',
-        image: i,
+        image: q,
         genre: 'Action-Adventure',
         discription: 'Return to Hyrule to awaken a sleeping princess and to recover the third triforce.'        
     },
+    {
+        id: 17,
+        name: 'Mega Man',
+        image: r,
+        genre: 'Shoot\'em Up',
+        discription: 'Mega Mans off to fight Dr. Wily and his robots.'        
+    },
+    {
+        id: 18,
+        name: 'Mega Man 2',
+        image: s,
+        genre: 'Shoot\'em Up',
+        discription: 'Mega Mans returns to defeat Dr. Wily and more of his robots.'        
+    },
+    {
+        id: 19,
+        name: 'Mega Man 3',
+        image: t,
+        genre: 'Shoot\'em Up',
+        discription: 'Mega Mans third installment aginst more of Dr. Wily\'s robots.'        
+    },
+    {
+        id: 20,
+        name: 'Mega Man 6',
+        image: u,
+        genre: 'Shoot\'em Up',
+        discription: 'Mega Mans sixth adventure with new power ups and abilitys to us agisnt more of Dr. Wily\'s bots.'        
+    },
+    {
+        id: 21,
+        name: 'Metroid',
+        image: v,
+        genre: 'Action-Adventure',
+        discription: 'Samuse travels to Zebes to fight against Mother Brings in her first game.'        
+    },
+    {
+        id: 22,
+        name: 'Mike Tyson\'s Punch Out!!',
+        image: w,
+        genre: 'Fighting',
+        discription: 'Play as Little Mac as he rise thought the ranks to reach the champ.'        
+    },
+    {
+        id: 23,
+        name: 'Radracer',
+        image: x,
+        genre: 'Racer',
+        discription: 'Drive eight courses in this NES classic.'        
+    },
+    {
+        id: 24,
+        name: 'Super C',
+        image: y,
+        genre: 'Shoot\'em Up',
+        discription: 'The shoot\'em up classic returns in this sequel.'        
+    },
+    {
+        id: 25,
+        name: 'Super Mario Bros.',
+        image: z,
+        genre: 'Platformer',
+        discription: 'Mario\'s first adventure to save Princess Toadstool from King Koopa.'        
+    },
+    {
+        id: 26,
+        name: 'Super Mario Bros. 2',
+        image: aa,
+        genre: 'Platformer',
+        discription: 'Mario reutrn with 3 of his friends to save dreamland from Wart.'        
+    },
+    {
+        id: 27,
+        name: 'Super Mario Bros. 3',
+        image: bb,
+        genre: 'Platformer',
+        discription: 'Mario\'s off to save Princess Toadstool and the seven kings from King Koopa and his children.'        
+    },
+    {
+        id: 28,
+        name: 'Tecmo Super Bowl',
+        image: cc,
+        genre: 'Sports',
+        discription: 'Get ready for some football in this NES classic.'        
+    },
+    {
+        id: 29,
+        name: 'Teenage Mutant Ninja Turtles',
+        image: dd,
+        genre: 'Platformer',
+        discription: 'The Turtles are off to save April and retieve the Life Transformer Gun to turn Splinter back to a humanbeing.'        
+    },
+    {
+        id: 30,
+        name: 'Tetris 2',
+        image: ee,
+        genre: 'Puzzle',
+        discription: 'Sequel to the classic puzzle game but with new game modes.'        
+    },
+    {
+        id: 31,
+        name: 'World Class Track Meet',
+        image: ff,
+        genre: 'Sports',
+        discription: 'Six olympic style games to play.'        
+    }
 ];
