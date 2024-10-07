274367a7c7e21838ae48d2d33abf3826bfbaf82c
Spencer Klessens <klessens92@gmail.com>
2024-10-07 18:48:19 -0500

    genesis game images are impoted in GENESISGAMES.js

--- src/app/shared/GENESISGAMES.js
+++ src/app/shared/GENESISGAMES.js
@@ -1,4 +1,15 @@
 import nintendoLogo from "../assets/images/Sega/gensesis/genesis.webp";
+import a from "../assets/images/Sega/gensesis/genesis_6pak.jpg"
+import b from "../assets/images/Sega/gensesis/genesis_alteredbeast.jpg"
+import c from "../assets/images/Sega/gensesis/genesis_columns.jpg"
+import d from "../assets/images/Sega/gensesis/genesis_mortalkombat.jpg"
+import e from "../assets/images/Sega/gensesis/genesis_sonicandknuckles.jpg"
+import f from "../assets/images/Sega/gensesis/genesis_sonicspinball.jpg"
+import g from "../assets/images/Sega/gensesis/genesis_sonicthehedgehog.jpg"
+import h from "../assets/images/Sega/gensesis/genesis_sonicthehedgehog2.jpg"
+import i from "../assets/images/Sega/gensesis/genesis_sonicthehedgehog3.jpg"
+import j from "../assets/images/Sega/gensesis/genesis_toejamandearl_2.jpg"
+import k from "../assets/images/Sega/gensesis/genesis_xmen2clonewars.jpg"
 
 export const GENESIS = [
     {
@@ -20,27 +31,27 @@ export const GENESIS = [
         name: 'GameThree',
         image: nintendoLogo,
         genre: 'rpg',
-        discription: 'ccc'      
+        discription: 'ccc'
     },
     {
         id: 3,
         name: 'GameFour',
         image: nintendoLogo,
         genre: 'puzzle',
-        discription: 'ddd'        
+        discription: 'ddd'
     },
     {
         id: 4,
         name: 'GameFive',
         image: nintendoLogo,
         genre: 'platformer',
-        discription: 'eee'        
+        discription: 'eee'
     },
     {
         id: 5,
         name: 'GameSix',
         image: nintendoLogo,
         genre: 'rpg',
-        discription: 'fff'        
+        discription: 'fff'
     },
 ];
