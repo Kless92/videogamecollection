4ad583c5eb794dbdc81fb531d3451c5ad9a0aac9
Spencer Klessens <klessens92@gmail.com>
2024-10-10 16:50:37 -0500

    images for own dreamcast games are added to image folder and DREAMCASTGAMES.js

--- src/app/shared/DREAMCASTGAMES.js
+++ src/app/shared/DREAMCASTGAMES.js
@@ -1,46 +1,84 @@
 import nintendoLogo from "../assets/images/Sega/dreamcast/dreamcast.png";
+import a from "../assets/images/Sega/dreamcast/dc_crazytaxi_US.16222790181674605812.jpg"
+import b from "../assets/images/Sega/dreamcast/dc_jetgrindradio_US.16891879591809480448.jpg"
+import c from "../assets/images/Sega/dreamcast/dc_MDK2_front.jpg"
+import d from "../assets/images/Sega/dreamcast/dc_rayman2_6.jpg"
+import e from "../assets/images/Sega/dreamcast/dc_residentevilcodeveronica_10.jpg"
+import f from "../assets/images/Sega/dreamcast/dc_shenmue_3.jpg"
+import g from "../assets/images/Sega/dreamcast/dc_sonicadventure_10.jpg"
+import h from "../assets/images/Sega/dreamcast/dc_soulcalibur_front.jpg"
+import i from "../assets/images/Sega/dreamcast/dc_spacechannel5_3.jpg"
+import j from "../assets/images/Sega/dreamcast/dc_thehouseofthedead2_5.jpg"
 
 export const DREAMCAST = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: 'Crazy Taxi',
+        image: a,
+        genre: 'Driving',
+        discription: 'Drive passengers to their destination before the timer runs out.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'Jet Grind Radio',
+        image: b,
+        genre: 'Platformer',
+        discription: 'Make your mark across the city and avoid the police.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'      
+        name: 'MDK2',
+        image: c,
+        genre: 'Third Person Shooter',
+        discription: 'Play 3 character with their own play style as you save the earth from aliens.'      
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'        
+        name: 'Rayman 2',
+        image: d,
+        genre: 'Platformer',
+        discription: 'Rayman returns to save his home from pirates.'        
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Resident Evil Code: Veronica',
+        image: e,
+        genre: 'Survival Horror',
+        discription: 'Play as Claire Redfield as she stuck on a prison island infested with zombies.'        
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'fff'        
+        name: 'Shenmue',
+        image: f,
+        genre: 'Action-Adventrue',
+        discription: 'Investigate your father\'s murder while fighting multiple opponents.'        
     },
+    {
+        id: 6,
+        name: 'Sonic Adventure',
+        image: g,
+        genre: 'Platformer',
+        discription: 'Sonic\'s new adventure while you play his story and five others.'        
+    },
+    {
+        id: 7,
+        name: 'Soul Calibur',
+        image: h,
+        genre: 'Fighting',
+        discription: 'One-on-one fights to clam the Soul Edge.'        
+    },
+    {
+        id: 8,
+        name: 'Space Channel 5',
+        image: i,
+        genre: 'Rhythm',
+        discription: 'Dance off aginst alian invaders and save hostages.'        
+    },
+    {
+        id: 9,
+        name: 'The House of the Dead',
+        image: j,
+        genre: 'Rail Shooter',
+        discription: 'Shoot your way through hordes of zombie and other monsters.'        
+    }
 ];
