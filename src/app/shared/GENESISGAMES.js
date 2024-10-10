7897e80d693db7ec53c1c5f8b124c565d74d0aa4
Spencer Klessens <klessens92@gmail.com>
2024-10-10 13:14:23 -0500

    images edit and all games are added to GENESISGAMES.js

--- src/app/shared/GENESISGAMES.js
+++ src/app/shared/GENESISGAMES.js
@@ -14,44 +14,79 @@ import k from "../assets/images/Sega/gensesis/genesis_xmen2clonewars.jpg"
 export const GENESIS = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: '6Pak',
+        image: a,
+        genre: 'Arcade',
+        discription: 'Six game in one: Sonic, Columns, Streets of rage, Golden Axe, Revenge of Shinobi and Super Hang-On.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'Altered Beast',
+        image: b,
+        genre: 'Beat\'em Up',
+        discription: 'Fight undead and the demonic god Neff as you trasform into a beast.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'
+        name: 'Columns',
+        image: c,
+        genre: 'Puzzle',
+        discription: 'Match color gem vertical, horizontal or diangle.'
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'
+        name: 'Mortal Kombat',
+        image: d,
+        genre: 'Fighting',
+        discription: 'Sega port of the arcade with you favorit characters and there fatalities.'
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'
+        name: 'Sonic and Knuckles',
+        image: e,
+        genre: 'Platformer',
+        discription: 'Sonic and Knuckles go up aginst Dr. Robotnik on Angel Island.'
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'fff'
+        name: 'Sonic Spin Ball',
+        image: f,
+        genre: 'Arcade',
+        discription: 'Sonic own pin ball game on sega genesis.'
+    },
+    {
+        id: 6,
+        name: 'Sonic the Hedgehog',
+        image: g,
+        genre: 'Platformer',
+        discription: 'Sonic first advengure saving the animal from Dr. Robotnik\'s machines.'
+    },
+    {
+        id: 7,
+        name: 'Sonic the Hedgehog 2',
+        image: h,
+        genre: 'Platformer',
+        discription: 'Sonic returns with a new friend to stop Dr. Robotnik.'
+    },
+    {
+        id: 8,
+        name: 'Sonic the Hedgehog 3',
+        image: i,
+        genre: 'Platformer',
+        discription: 'Sonic returns again to stop Dr. Robotnik and a new threat Knuckles.'
+    },
+    {
+        id: 9,
+        name: 'Toejam and Earl',
+        image: j,
+        genre: 'Platformer',
+        discription: 'These two crash on earth and now have to find your ship pieces.'
+    },
+    {
+        id: 10,
+        name: 'X-Men 2: Clone Wars',
+        image: k,
+        genre: 'Platformer',
+        discription: 'The X-Men returns to fight aginst Phalanx.'
     },
 ];
