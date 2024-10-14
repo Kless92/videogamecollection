d001ce441e12ca55568d105bea5870429115c8af
Spencer Klessens <klessens92@gmail.com>
2024-10-14 13:15:05 -0500

    New image for Atari 2600 added and image imports onto ATARI2600GAMES.js

--- src/app/shared/ATARI2600GAMES.js
+++ src/app/shared/ATARI2600GAMES.js
@@ -1,4 +1,28 @@
 import nintendoLogo from "../assets/images/Atari/atari2600.png";
+import a from "../assets/images/Atari/a2600_adventure.jpg"
+import b from "../assets/images/Atari/a2600_asteroids.jpg"
+import c from "../assets/images/Atari/a2600_basketball.jpg"
+import d from "../assets/images/Atari/a2600_berzerk.jpg"
+import e from "../assets/images/Atari/a2600_breakout.jpg"
+import f from "../assets/images/Atari/a2600_choppercommand.jpg"
+import g from "../assets/images/Atari/a2600_circusatari.jpg"
+import h from "../assets/images/Atari/a2600_combatvcs.jpg"
+import i from "../assets/images/Atari/a2600_defender.jpg"
+import j from "../assets/images/Atari/a2600_demonattack.jpg"
+import k from "../assets/images/Atari/a2600_demonstodiamonds.jpg"
+import l from "../assets/images/Atari/a2600_football.jpg"
+import m from "../assets/images/Atari/a2600_frogger.jpg"
+import n from "../assets/images/Atari/a2600_kaboom.jpg"
+import o from "../assets/images/Atari/a2600_missilecommand.jpg"
+import p from "../assets/images/Atari/a2600_nightdriver.jpg"
+import q from "../assets/images/Atari/a2600_pacmanvcs.jpg"
+import r from "../assets/images/Atari/a2600_pitfall.jpg"
+import s from "../assets/images/Atari/a2600_qbert.jpg"
+import t from "../assets/images/Atari/a2600_spaceinvaders.jpg"
+import u from "../assets/images/Atari/a2600_starwarsdeathstarbattle.jpg"
+import v from "../assets/images/Atari/a2600_videoolympics.jpg"
+import w from "../assets/images/Atari/a2600_warlords.jpg"
+import x from "../assets/images/Atari/a2600_yarsrevenge.jpg"
 
 export const ATARI2600 = [
     {
