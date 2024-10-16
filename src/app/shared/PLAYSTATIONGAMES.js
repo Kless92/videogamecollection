8d321a97afb730975266c56393ef23668133c1d9
Spencer Klessens <klessens92@gmail.com>
2024-10-16 13:39:38 -0500

    new images added to images/sony/ps1 and image imported to PLAYSTATIONGAMES.js

--- src/app/shared/PLAYSTATIONGAMES.js
+++ src/app/shared/PLAYSTATIONGAMES.js
@@ -1,46 +1,196 @@
 import nintendoLogo from "../assets/images/Sony/ps1/ps1.png";
+import a from "../assets/images/Sony/ps1/ps1_castlevaniasymphonyofthenight.jpg"
+import b from "../assets/images/Sony/ps1/ps1_crashbandicoot.jpg"
+import c from "../assets/images/Sony/ps1/ps1_davemirrafreestylebmx.jpg"
+import d from "../assets/images/Sony/ps1/ps1_ecw_hardcore_revolution.jpg"
+import e from "../assets/images/Sony/ps1/ps1_finalfantasyvii_13.jpg"
+import f from "../assets/images/Sony/ps1/ps1_finalfantasyix_12.jpg"
+import g from "../assets/images/Sony/ps1/ps1_metalgearsolid_front.jpg"
+import h from "../assets/images/Sony/ps1/ps1_namco_museum_vol_3.jpg"
+import i from "../assets/images/Sony/ps1/ps1_nba_live_96.jpg"
+import j from "../assets/images/Sony/ps1/ps1_rayman.jpg"
+import k from "../assets/images/Sony/ps1/ps1_residentevildirectorscut_3.jpg"
+import l from "../assets/images/Sony/ps1/ps1_resident_evil_3_nemesis.jpg"
+import m from "../assets/images/Sony/ps1/ps1_rugrats_search_for_reptar.png"
+import n from "../assets/images/Sony/ps1/ps1_silenthill.jpg"
+import o from "../assets/images/Sony/ps1/ps1_spyrothedragon_2.jpg"
+import p from "../assets/images/Sony/ps1/ps1_spyro2riptosrage.jpg"
+import q from "../assets/images/Sony/ps1/ps1_tombraider.jpg"
+import r from "../assets/images/Sony/ps1/ps1_tombraider2.jpg"
+import s from "../assets/images/Sony/ps1/ps1_tombraider3.jpg"
+import t from "../assets/images/Sony/ps1/ps1_tonyhawksproskater.jpg"
+import u from "../assets/images/Sony/ps1/ps1_tonyhawksproskater2.jpg"
+import v from "../assets/images/Sony/ps1/ps1_tonyhawksproskater3.jpg"
+import w from "../assets/images/Sony/ps1/ps1_toystory2.jpg"
+import x from "../assets/images/Sony/ps1/ps1_wwfsmackdown2_eu.jpg"
 
 export const PLAYSTATION = [
     {
         id: 0,
         name: 'GameOne',
-        image: nintendoLogo,
+        image: a,
         genre: 'platformer',
         discription: 'aaa'
     },
     {
         id: 1,
         name: 'GameTwo',
-        image: nintendoLogo,
+        image: b,
         genre: 'shooter',
         discription: 'bbb'
     },
     {
         id: 2,
         name: 'GameThree',
-        image: nintendoLogo,
+        image: c,
         genre: 'rpg',
         discription: 'ccc'      
     },
     {
         id: 3,
         name: 'GameFour',
-        image: nintendoLogo,
+        image: d,
         genre: 'puzzle',
         discription: 'ddd'        
     },
     {
         id: 4,
         name: 'GameFive',
-        image: nintendoLogo,
+        image: e,
         genre: 'platformer',
         discription: 'eee'        
     },
     {
         id: 5,
         name: 'GameSix',
-        image: nintendoLogo,
+        image: f,
         genre: 'rpg',
         discription: 'fff'        
     },
+    {
+        id: 6,
+        name: 'GameSix',
+        image: g,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 7,
+        name: 'GameSix',
+        image: h,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 8,
+        name: 'GameSix',
+        image: i,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 9,
+        name: 'GameSix',
+        image: j,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 10,
+        name: 'GameSix',
+        image: k,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 11,
+        name: 'GameSix',
+        image: l,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 12,
+        name: 'GameSix',
+        image: m,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 13,
+        name: 'GameSix',
+        image: n,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 14,
+        name: 'GameSix',
+        image: o,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 15,
+        name: 'GameSix',
+        image: p,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 16,
+        name: 'GameSix',
+        image: q,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 17,
+        name: 'GameSix',
+        image: r,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 18,
+        name: 'GameSix',
+        image: s,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 19,
+        name: 'GameSix',
+        image: t,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 20,
+        name: 'GameSix',
+        image: u,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 21,
+        name: 'GameSix',
+        image: v,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 22,
+        name: 'GameSix',
+        image: w,
+        genre: 'rpg',
+        discription: 'fff'        
+    },
+    {
+        id: 23,
+        name: 'GameSix',
+        image: x,
+        genre: 'rpg',
+        discription: 'fff'        
+    }
 ];
