2aff5b1832d3c237996a632c97cf2d8f20450be0
Spencer Klessens <klessens92@gmail.com>
2024-10-21 13:45:33 -0500

    all images of ps2, ps3, ps4 and ps5 are edited and PLAYSTATION3 4 AND 5.js have all own game added.

--- src/app/shared/PLAYSTATION3GAMES.js
+++ src/app/shared/PLAYSTATION3GAMES.js
@@ -1,46 +1,68 @@
 import nintendoLogo from "../assets/images/Sony/ps3/ps3.png";
+import dantesInferno from "../assets/images/Sony/ps3/ps3_dantesinferno.jpg"
+import demonSouls from "../assets/images/Sony/ps3/ps3_demonssouls_3.jpg"
+import godOfWar3 from "../assets/images/Sony/ps3/ps3_godofwariii.jpg"
+import icoShadowOfTheColossus from "../assets/images/Sony/ps3/ps3_icoshadowofthecolossus.jpg"
+import infamous from "../assets/images/Sony/ps3/ps3_infamous.jpg"
+import mGS4 from "../assets/images/Sony/ps3/ps3_mgs4gunsofthepatriots_8.jpg"
+import unCharted1 from "../assets/images/Sony/ps3/ps3_uncharteddrakesfortune.jpg"
+import unCharted2 from "../assets/images/Sony/ps3/ps3_uncharted2amongthieves.jpg"
 
 export const PLAYSTATION3 = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: 'Dantes Inferno',
+        image: dantesInferno,
+        genre: 'Action-Adventure',
+        discription: 'Descend into Hell to save your deceased wife soul.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'Demon Souls',
+        image: demonSouls,
+        genre: 'Action Role-Playing',
+        discription: 'Enter Boletaria and save the land from demon in the first "souls" game.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'      
+        name: 'God Of War III',
+        image: godOfWar3,
+        genre: 'Action-Adventure',
+        discription: 'Kratos journey concludes as the battle to Gods on Mount Olympus.'      
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'        
+        name: 'Icos & Shadow of the Colossus',
+        image: icoShadowOfTheColossus,
+        genre: 'Action-Adventure',
+        discription: 'Remaster of the PS2 classics bundle into one game.'        
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Infamouse',
+        image: infamous,
+        genre: 'Action-Adventure',
+        discription: 'Cole MacGrath gains the power of electricity and is off to set the fate of the city.'        
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'fff'        
+        name: 'Metal Gear Soild 4: Guns of the Patriots',
+        image: mGS4,
+        genre: 'Action-Adventure',
+        discription: 'Solid Snake return to complete his last mission before his illness take him.'        
+    },
+    {
+        id: 6,
+        name: 'Uncharted Drakes Fortune',
+        image: unCharted1,
+        genre: 'Action-Adventure',
+        discription: 'Nathan off to find his Fortune.'        
+    },
+    {
+        id: 7,
+        name: 'Uncharted 2: Among Thieves',
+        image: unCharted2,
+        genre: 'Action-Adventure',
+        discription: 'Nathan adventure continues when he task to steal agent treasure.'        
     },
 ];
