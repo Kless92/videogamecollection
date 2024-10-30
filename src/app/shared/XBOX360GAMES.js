d668bbf734da72aaa5f20e6043af262f7cd07fbe
Spencer Klessens <klessens92@gmail.com>
2024-10-30 15:11:45 -0500

    forgot images added to Microsoft/xbox360 and all own game are added to XBOX360GAMES.js

--- src/app/shared/XBOX360GAMES.js
+++ src/app/shared/XBOX360GAMES.js
@@ -1,10 +1,46 @@
-import nintendoLogo from "../assets/images/Microsoft/xbox360/Xbox360.png";
 import assassinsCreedIII from "../assets/images/Microsoft/xbox360/x360_assassinscreediii.jpg";
 import assassinsCreedRevelations from "../assets/images/Microsoft/xbox360/x360_assassinscreedrevelations.jpg";
 import batManArkamAsylum from "../assets/images/Microsoft/xbox360/x360_batmanarkhamasylum.jpg";
 import battleField3 from "../assets/images/Microsoft/xbox360/x360_battlefield3.jpg";
 import bioshock from "../assets/images/Microsoft/xbox360/x360_bioshock.jpg";
 import bioshock2 from "../assets/images/Microsoft/xbox360/x360_bioshock.jpg";
+import bioshockInfinite from "../assets/images/Microsoft/xbox360/x360_bioshockinfinite.jpg"
+import borderLands from "../assets/images/Microsoft/xbox360/x360_borderlands.jpg"
+import borderLands2 from "../assets/images/Microsoft/xbox360/x360_borderlands2.jpg"
+import brutalLegend from "../assets/images/Microsoft/xbox360/x360_brutallegend.jpg"
+import callOfDutyBlackOps from "../assets/images/Microsoft/xbox360/x360_callofdutyblackops.jpg"
+import callOfDutyMW3 from "../assets/images/Microsoft/xbox360/x360_callofdutymodernwarfare3.jpg"
+import callOfDutyBlackOps2 from "../assets/images/Microsoft/xbox360/x360_callofdutyblackopsii.jpg"
+import callOfDutyGhost from "../assets/images/Microsoft/xbox360/x360_callofdutyghosts.jpg"
+import darkSiders from "../assets/images/Microsoft/xbox360/x360_darksiders.jpg"
+import darkSiders2 from "../assets/images/Microsoft/xbox360/x360_darksidersii.jpg"
+import deadIsland from "../assets/images/Microsoft/xbox360/x360_deadisland.jpg"
+import deadSpace from "../assets/images/Microsoft/xbox360/x360_deadspace.jpg"
+import destiny from "../assets/images/Microsoft/xbox360/x360_destiny.jpg"
+import dragonAgeOrigins from "../assets/images/Microsoft/xbox360/x360_dragonageorigins.jpg"
+import elderScrolls5Skyrim from "../assets/images/Microsoft/xbox360/x360_elderscrollsvskyrim_the.jpg"
+import fable2 from "../assets/images/Microsoft/xbox360/x360_fable2.jpg"
+import fable3 from "../assets/images/Microsoft/xbox360/x360_fable3.jpg"
+import falloutNewVegas from "../assets/images/Microsoft/xbox360/x360_falloutnewvegas.jpg"
+import gearsOfWar from "../assets/images/Microsoft/xbox360/x360_gearsofwar.jpg"
+import gearsOfWar2 from "../assets/images/Microsoft/xbox360/x360_gearsofwar2.jpg"
+import gearsOfWar3 from "../assets/images/Microsoft/xbox360/x360_gearsofwar3.jpg"
+import gearsOfWarJudgment from "../assets/images/Microsoft/xbox360/x360_gearsofwarjudgment.jpg"
+import grandTheftAuto4 from "../assets/images/Microsoft/xbox360/x360_grandtheftauto4.jpg"
+import grandTheftAuto5 from "../assets/images/Microsoft/xbox360/x360_grandtheftauto5.jpg"
+import gutarHero2 from "../assets/images/Microsoft/xbox360/x360_guitarheroii.jpg"
+import gutarHero3 from "../assets/images/Microsoft/xbox360/x360_guitarheroiii.jpg"
+import halo3 from "../assets/images/Microsoft/xbox360/x360_halo3.jpg"
+import halo3ODST from "../assets/images/Microsoft/xbox360/x360_halo3odst.jpg"
+import haloReach from "../assets/images/Microsoft/xbox360/x360_haloreach.jpg"
+import haloCombatEvolvedAnniversary from "../assets/images/Microsoft/xbox360/x360_halocombatevolvedanniversary.jpg"
+import halo4 from "../assets/images/Microsoft/xbox360/x360_halo4.jpg"
+import massEffect from "../assets/images/Microsoft/xbox360/x360_masseffect.jpg"
+import theOrangeBox from "../assets/images/Microsoft/xbox360/x360_theorangebox.jpg"
+import rage from "../assets/images/Microsoft/xbox360/x360_rage.jpg"
+import redDeadRedemption from "../assets/images/Microsoft/xbox360/x360_reddeadredemption.jpg"
+import residentEvil5 from "../assets/images/Microsoft/xbox360/x360_residentevil5.jpg"
+import rockBand from "../assets/images/Microsoft/xbox360/x360_rockband.jpg"
 
 export const XBOX360 = [
     {
@@ -47,6 +83,265 @@ export const XBOX360 = [
         name: 'Bioshock 2',
         image: bioshock2,
         genre: 'First-Person Shooter',
+        discription: 'Return to Rapture as a big daddy name Delta as he\'s off to save his little sister.'        
+    },
+    {
+        id: 6,
+        name: 'Bioshock Infinite',
+        image: bioshockInfinite,
+        genre: 'First-Person Shooter',
+        discription: 'Reach the sky to Columbia were your task to find Elizabeth to wipe away your debt.'        
+    },
+    {
+        id: 7,
+        name: 'Borderlands',
+        image: borderLands,
+        genre: 'First-Person Shooter',
+        discription: 'Travel to Pandora and shoot your way through everything to reach the Vault.'        
+    },
+    {
+        id: 8,
+        name: 'Borderlands 2',
+        image: borderLands2,
+        genre: 'First-Person Shooter',
+        discription: 'Return to Pandora and fight against Handsome Jack and everything against you.'        
+    },
+    {
+        id: 9,
+        name: 'Brutal Legend',
+        image: brutalLegend,
+        genre: 'Real-Time Strategy',
+        discription: 'Play as Eddie Riggs as he sent to inspired by Heavy Metal.'        
+    },
+    {
+        id: 10,
+        name: 'Call of Duty: Black Ops',
+        image: callOfDutyBlackOps,
+        genre: 'First-Person Shooter',
+        discription: 'Enter to the 60\'s in Vietnam to stop the Soviet.'        
+    },
+    {
+        id: 11,
+        name: 'Call of Duty: Monder Warfare 3',
+        image: callOfDutyMW3,
+        genre: 'First-Person Shooter',
+        discription: 'Enter the battle grounds of World War III to find down Nikolai.'        
+    },
+    {
+        id: 12,
+        name: 'Call of Duty: Black Ops 2',
+        image: callOfDutyBlackOps2,
+        genre: 'First-Person Shooter',
+        discription: 'Travle to the 80\'s and a fictional 2025 to stop the terrorist Raul Menendez.'        
+    },
+    {
+        id: 13,
+        name: 'Call of Duty: Ghost',
+        image: callOfDutyGhost,
+        genre: 'First-Person Shooter',
+        discription: 'In an alternate world a military dictatorship, who has control over South, Center and parts of North America, are at war witht he United States.'        
+    },
+    {
+        id: 14,
+        name: 'Dark Siders',
+        image: darkSiders,
+        genre: 'Action-Adventure',
+        discription: 'The battle between Heaven and Hell has begun and War is accused of starting it.'        
+    },
+    {
+        id: 15,
+        name: 'Dark Siders II',
+        image: darkSiders2,
+        genre: 'Action-Adventure',
+        discription: 'Death travel a war torn world to clear Wars name and restor the world.'        
+    },
+    {
+        id: 16,
+        name: 'Dead Island',
+        image: deadIsland,
+        genre: 'Survival Horror',
+        discription: 'Most of the population on a island resort is turn into zombie and you must survive.'        
+    },
+    {
+        id: 17,
+        name: 'Deadspace',
+        image: deadSpace,
+        genre: 'Survival Horror',
+        discription: 'Navigate through the Ishimura, a space mining colony ship filled with monsters from your worst nightmare.'        
+    },
+    {
+        id: 18,
+        name: 'Destiny',
+        image: destiny,
+        genre: 'First-Person Shooter',
+        discription: 'Travel from Earth, the Moon, Venus and Mars to fight aliens and the darkness.'        
+    },
+    {
+        id: 19,
+        name: 'Dragon Age: Origins',
+        image: dragonAgeOrigins,
+        genre: 'Role-Playing Game',
         discription: 'Return ot Rapture as a big daddy name Delta as he\'s off to save his little sister.'        
     },
+    {
+        id: 20,
+        name: 'Elder Scrolls 5: Skyrim',
+        image: elderScrolls5Skyrim,
+        genre: 'Action Role-Playing',
+        discription: 'Play as the Dragon Born as you travel the province of Skyrim fighting dragons.'        
+    },
+    {
+        id: 21,
+        name: 'Fable 2',
+        image: fable2,
+        genre: 'Action Role-Playing',
+        discription: 'Avenge your sister and save the world with might, skills and magic.'        
+    },
+    {
+        id: 22,
+        name: 'Fable 3',
+        image: fable3,
+        genre: 'Action Role-Playing',
+        discription: 'Save Albion from your corrupted brother by a revolution.'        
+    },
+    {
+        id: 23,
+        name: 'Fallout: New Vegas',
+        image: falloutNewVegas,
+        genre: 'Action Role-Playing',
+        discription: 'Navigate the Mojave Desert to find your killer and decide who controls New Vegas.'        
+    },
+    {
+        id: 24,
+        name: 'Gears of War',
+        image: gearsOfWar,
+        genre: 'Third-Person Shooter',
+        discription: 'Fight against the Locust in the remnants of a torn world.'        
+    },
+    {
+        id: 25,
+        name: 'Gears of War 2',
+        image: gearsOfWar2,
+        genre: 'Third-Person Shooter',
+        discription: 'The fight continues as you travel to the Locust home front.'        
+    },
+    {
+        id: 26,
+        name: 'Gears of War 3',
+        image: gearsOfWar3,
+        genre: 'Third-Person Shooter',
+        discription: 'The Locust is defeated but a new threat arrives known as the lamb.'        
+    },
+    {
+        id: 27,
+        name: 'Gears of War: Judgment',
+        image: gearsOfWarJudgment,
+        genre: 'Third-Person Shooter',
+        discription: 'See the past of Kilo and his squad in the middle of war.'        
+    },
+    {
+        id: 28,
+        name: 'Grand Theft Auto 4',
+        image: grandTheftAuto4,
+        genre: 'Action-Adventure',
+        discription: 'Return to Liberty City as Niko Bellic as he makes it big and get involved with many characters.'        
+    },
+    {
+        id: 29,
+        name: 'Grand Theft Auto 5',
+        image: grandTheftAuto5,
+        genre: 'Action-Adventure',
+        discription: 'Return to Los Santos as three character as they turn the city to their own playground.'        
+    },
+    {
+        id: 30,
+        name: 'Gutar Hero 2',
+        image: gutarHero2,
+        genre: 'Rhythm',
+        discription: 'Sequel to Guitar Hero including more songs and unlockable characters.'        
+    },
+    {
+        id: 31,
+        name: 'Gutar Hero 3',
+        image: gutarHero3,
+        genre: 'Rhythm',
+        discription: 'Gutar Hero returns with a new carrior story and even more song to rock out.'        
+    },
+    {
+        id: 32,
+        name: 'Halo 3',
+        image: halo3,
+        genre: 'First-Person Shooter',
+        discription: 'Mast Chiefs last fight against the Covenant and the Flood.'        
+    },
+    {
+        id: 33,
+        name: 'Halo 3 ODST',
+        image: halo3ODST,
+        genre: 'First-Person Shooter',
+        discription: 'A ODST soldiers navigate New Mombasa to find his missing team mate during the events of Halo 2.'        
+    },
+    {
+        id: 34,
+        name: 'Halo Reach',
+        image: haloReach,
+        genre: 'First-Person Shooter',
+        discription: 'See the fall of Reach as Nole Team fight to push the Covenant back.'        
+    },
+    {
+        id: 35,
+        name: 'Halo Combat Evolved Anniversary',
+        image: haloCombatEvolvedAnniversary,
+        genre: 'First-Person Shooter',
+        discription: 'Remaster of Halo Combat Evolved which always players to switch between the old and new graphics.'        
+    },
+    {
+        id: 36,
+        name: 'Halo 4',
+        image: halo4,
+        genre: 'First-Person Shooter',
+        discription: 'Master Chief returns as he fight a new enemy.'        
+    },
+    {
+        id: 37,
+        name: 'Mass Effect',
+        image: massEffect,
+        genre: 'Action Role-Playing',
+        discription: 'Commander Shepard must save galaxy from the Reapers as a Paragon or Renegade.'        
+    },
+    {
+        id: 38,
+        name: 'The Orange Box',
+        image: theOrangeBox,
+        genre: 'Various',
+        discription: 'A collection of Valve games including: Half-Life 2 episode 1 and 2, Team Fortress 2 and Portal.'        
+    },
+    {
+        id: 35,
+        name: 'Rage',
+        image: rage,
+        genre: 'First-Person Shooter',
+        discription: 'Wake up to a dystopian future after an asteroid turn earth into a wasteland.'        
+    },
+    {
+        id: 35,
+        name: 'Red Dead Redemption',
+        image: redDeadRedemption,
+        genre: 'Action-Adventure',
+        discription: 'John Marston must take out his old crew to save his wife and son 1900 old west.'        
+    },
+    {
+        id: 35,
+        name: 'Resident Evil 5',
+        image: residentEvil5,
+        genre: 'Third-Person Shooter',
+        discription: 'Christ Redfield returns, teaming up with Sheva Alomar to stop a BOW threat in West Africa.'        
+    },
+    {
+        id: 35,
+        name: 'Rock Band',
+        image: rockBand,
+        genre: 'Rhythm',
+        discription: 'Play rock song with friends on guitar, bass, drums or vocals.'        
+    }
 ];
