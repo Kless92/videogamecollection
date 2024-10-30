bce2583d63000929aaf65b4aedb0e3f1a1c4499c
Spencer Klessens <klessens92@gmail.com>
2024-10-29 19:24:15 -0500

    all xbox360 images are edited and first 6 are in XBOX360GAMES.JS

--- src/app/shared/XBOX360GAMES.js
+++ src/app/shared/XBOX360GAMES.js
@@ -1,46 +1,52 @@
 import nintendoLogo from "../assets/images/Microsoft/xbox360/Xbox360.png";
+import assassinsCreedIII from "../assets/images/Microsoft/xbox360/x360_assassinscreediii.jpg";
+import assassinsCreedRevelations from "../assets/images/Microsoft/xbox360/x360_assassinscreedrevelations.jpg";
+import batManArkamAsylum from "../assets/images/Microsoft/xbox360/x360_batmanarkhamasylum.jpg";
+import battleField3 from "../assets/images/Microsoft/xbox360/x360_battlefield3.jpg";
+import bioshock from "../assets/images/Microsoft/xbox360/x360_bioshock.jpg";
+import bioshock2 from "../assets/images/Microsoft/xbox360/x360_bioshock.jpg";
 
 export const XBOX360 = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: 'Assassins Creed: Revelations',
+        image: assassinsCreedRevelations,
+        genre: 'Action-Adventure',
+        discription: 'Ezio\'s final adventure as he fight Templars in Constantinople.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'Assassins Creed: III',
+        image: assassinsCreedIII,
+        genre: 'Action-Adventure',
+        discription: 'Go back to 18th-century Colonial America as Ratonhnhaké:ton; a Native American seeking vengons for his tribe.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'      
+        name: 'Batman: Arkham Asylum',
+        image: batManArkamAsylum,
+        genre: 'Action-Adventure',
+        discription: 'The Joker has taken over Arkham  Asylum, no Batman must stop him and most of the villians he put away.'      
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'        
+        name: 'Battle Field 3',
+        image: battleField3,
+        genre: 'First-Person Shooter',
+        discription: 'The third Battle Field game as you battle on massive map with vehicle and air support and team roles.'        
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Bioshock',
+        image: bioshock,
+        genre: 'First-Person Shooter',
+        discription: 'Enter the under water city of Rapture as you survive the splices and Andrews Rhyons.'        
     },
     {
         id: 5,
-        name: 'GameSix',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'fff'        
+        name: 'Bioshock 2',
+        image: bioshock2,
+        genre: 'First-Person Shooter',
+        discription: 'Return ot Rapture as a big daddy name Delta as he\'s off to save his little sister.'        
     },
-];g
+];
