31a7ae1d56ea8442fcfc38c930641db94265b0e7
Spencer Klessens <klessens92@gmail.com>
2024-11-04 20:51:26 -0600

    New js added; CDROMGAMES.js

---
+++ src/app/shared/CDROMGAMES.js
@@ -0,0 +1,60 @@
+import nintendoLogo from "../assets/images/Microsoft/xbox360/Xbox360.png";
+
+export const CDROM = [
+    {
+        id: 0,
+        name: 'Diablo: Battle Chest',
+        image: nintendoLogo,
+        genre: 'Action Role-Playing Game',
+        discription: 'Contains Diable, Diable II and Diable II: Lord of Destruction'
+    },
+    {
+        id: 1,
+        name: 'Fallout 4',
+        image: nintendoLogo,
+        genre: 'Action Role-Playing Game',
+        discription: 'Venture though post-apocalyptic Bosten to find you son.'        
+    },
+    {
+        id: 2,
+        name: 'Lord of the Ring: The Fellowship of the Ring',
+        image: nintendoLogo,
+        genre: 'Action-Adventure',
+        discription: 'The first book of Lord of the Ring is transformed into a video game with some liberties.'
+    },
+    {
+        id: 3,
+        name: 'StarCraft Battle Chest',
+        image: nintendoLogo,
+        genre: 'Real-Tiem Strategy',
+        discription: 'Contains StarCraft and StarCraft: Brood War'      
+    },
+    {
+        id: 4,
+        name: 'World of Warcraft',
+        image: nintendoLogo,
+        genre: 'Massively Multiplayer Online',
+        discription: 'Blizzard massive open world, base on the Warcraft games, where two warring factions fight for Azeroth.'        
+    },
+    {
+        id: 5,
+        name: 'World of Warcraft: The Burning Crusade',
+        image: nintendoLogo,
+        genre: 'Massively Multiplayer Online',
+        discription: 'The Dark Portal has open and adventurers travel to Outland.'        
+    },
+    {
+        id: 6,
+        name: 'World of Warcraft: The Wrath of the Litch King',
+        image: nintendoLogo,
+        genre: 'Massively Multiplayer Online',
+        discription: 'Adventurers travle to Northland to fight aginst the Litch King.'        
+    },
+    {
+        id: 7,
+        name: 'World of Warcraft: Cataclysm',
+        image: nintendoLogo,
+        genre: 'Massively Multiplayer Online',
+        discription: 'Azeroth has changed forever as Deathwing returns and scared the world.'      
+    }
+]
