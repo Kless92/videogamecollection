1039d69d39f3e930d683a323d5b5497543cba1b1
Spencer Klessens <klessens92@gmail.com>
2024-11-06 13:22:51 -0600

    all image in gameboycolor, gameboyadvance, nintendods and nintendo3ds are edited and images of own cdrom games are added.

--- src/app/shared/CDROMGAMES.js
+++ src/app/shared/CDROMGAMES.js
@@ -10,48 +10,41 @@ export const CDROM = [
     },
     {
         id: 1,
-        name: 'Fallout 4',
-        image: nintendoLogo,
-        genre: 'Action Role-Playing Game',
-        discription: 'Venture though post-apocalyptic Bosten to find you son.'        
-    },
-    {
-        id: 2,
         name: 'Lord of the Ring: The Fellowship of the Ring',
         image: nintendoLogo,
         genre: 'Action-Adventure',
         discription: 'The first book of Lord of the Ring is transformed into a video game with some liberties.'
     },
     {
-        id: 3,
+        id: 2,
         name: 'StarCraft Battle Chest',
         image: nintendoLogo,
         genre: 'Real-Tiem Strategy',
         discription: 'Contains StarCraft and StarCraft: Brood War'      
     },
     {
-        id: 4,
+        id: 3,
         name: 'World of Warcraft',
         image: nintendoLogo,
         genre: 'Massively Multiplayer Online',
         discription: 'Blizzard massive open world, base on the Warcraft games, where two warring factions fight for Azeroth.'        
     },
     {
-        id: 5,
+        id: 4,
         name: 'World of Warcraft: The Burning Crusade',
         image: nintendoLogo,
         genre: 'Massively Multiplayer Online',
         discription: 'The Dark Portal has open and adventurers travel to Outland.'        
     },
     {
-        id: 6,
+        id: 5,
         name: 'World of Warcraft: The Wrath of the Litch King',
         image: nintendoLogo,
         genre: 'Massively Multiplayer Online',
         discription: 'Adventurers travle to Northland to fight aginst the Litch King.'        
     },
     {
-        id: 7,
+        id: 6,
         name: 'World of Warcraft: Cataclysm',
         image: nintendoLogo,
         genre: 'Massively Multiplayer Online',
---
+++ src/pages/Mis/cdromPage.js
@@ -0,0 +1,12 @@
+import { Container } from "reactstrap";
+
+const GameboyadvancePage = () => {
+    return (
+        <Container>
+            <h1>PC Games on CDROM Collection</h1>
+
+        </Container>
+    )
+}
+
+export default GameboyadvancePage;
