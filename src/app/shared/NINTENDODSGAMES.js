6db5adf33cd39dcb341c7f5d6c8c86a32d9ad8d9
Spencer Klessens <klessens92@gmail.com>
2024-11-08 13:47:13 -0600

    all info filled in NINTENDODSGAMES.js and new style put in App.css and GameDisplay.js for testing

--- src/app/shared/NINTENDODSGAMES.js
+++ src/app/shared/NINTENDODSGAMES.js
@@ -1,32 +1,35 @@
-import nintendoLogo from "../assets/images/Microsoft/xbox360/Xbox360.png";
+import zeldaPhatomHourGlass from "../assets/images/Nintendo/ds/ds_legendofzeldaphantomhourglass.jpg"
+import metroidPrimeHunters from "../assets/images/Nintendo/ds/ds_metroidprimehunters.jpg"
+import newSuperMarioBros from "../assets/images/Nintendo/ds/ds_newsupermariobros.jpg"
+import pokemonPearl from "../assets/images/Nintendo/ds/ds_pokemonpearlversion.jpg"
 
 export const NINTENDODS = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: 'The Legend of Zelda: Phatome Hour Class',
+        image: zeldaPhatomHourGlass,
+        genre: 'Action-Adventure',
+        discription: 'Link must save Tetra with the help of Captain Linebeck to sail to different islands.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'Metroid Prime: Hunters',
+        image: metroidPrimeHunters,
+        genre: 'Action-Adventure',
+        discription: 'Samus is off to retrieve this "Ultimate Power" will six other bounty hunter are after it as well.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'      
+        name: 'New Super Mario Bros.',
+        image: newSuperMarioBros,
+        genre: 'Platformer',
+        discription: 'Princess Peach is kidnapped again and it up to Mario and Luigi to save here again.'      
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'        
+        name: 'Pokemon Pearl',
+        image: pokemonPearl,
+        genre: 'Role-Playing Game',
+        discription: 'Fourth Generation of Pokemon takes place in the Sinnoh region as your off to discover and catch new Pokemon.'        
     }
 ]
--- src/features/Display/GameDisplay.js
+++ src/features/Display/GameDisplay.js
@@ -1,17 +1,21 @@
 import { Col, Row, Table } from "reactstrap";
 import { NavLink, Route, Routes, Link } from 'react-router-dom';
+
+
 const GameDisplay = ({items}) => {
     const { id, name, image, genre } = items;
     return (
-        <Col>
-            <Link to={`${id}`}>
-                <img src={image} alt={name} className="shrink"/>
-            </Link>
-                <br />
-                {name}
-                <br />
-                {genre}
-        </Col>
+        <Table style={{background:'white'}}>
+            <Col className="test">
+                <Link to={`${id}`} >
+                    <img src={image} alt={name} className="shrink"/>
+                </Link>
+                    <br />
+                    {name}
+                    <br />
+                    {genre}
+            </Col>
+        </Table>
     )
 };
 
