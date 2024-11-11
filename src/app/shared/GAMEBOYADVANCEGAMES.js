37c36d79e9491c32198126a12a6cba3bdd4fa239
Spencer Klessens <klessens92@gmail.com>
2024-11-11 13:07:26 -0600

    GAMEBOYADVANCEGAMES.js and PLAYSTATIONPORTABLEGAEMS.js is updated with all own game info and image imports.

--- src/app/shared/GAMEBOYADVANCEGAMES.js
+++ src/app/shared/GAMEBOYADVANCEGAMES.js
@@ -1,81 +1,91 @@
-import nintendoLogo from "../assets/images/Microsoft/xbox360/Xbox360.png";
+import donkeyKongCountry from "../assets/images/Nintendo/gameboyadvance/gba_donkeykongcountry.jpg"
+import donkeyKongCountry3 from "../assets/images/Nintendo/gameboyadvance/gba_donkeykongcountry3.jpg"
+import kingdomHeartsChainOfMemories from "../assets/images/Nintendo/gameboyadvance/gba_kingdomheartschainofmemories.jpg"
+import legendOfZelda from "../assets/images/Nintendo/gameboyadvance/gba_legendofzelda.jpg"
+import zeldaLinkToThePast from "../assets/images/Nintendo/gameboyadvance/gba_legendofzeldaalinktothepast.jpgg"
+import zeldaMinishCap from "../assets/images/Nintendo/gameboyadvance/gba_legendofzeldatheminishcap.jpg"
+import marioAndLuigiSuperStarSaga from "../assets/images/Nintendo/gameboyadvance/gba_marioandluigisuperstarsaga.jpg"
+import metroidZeroMission from "../assets/images/Nintendo/gameboyadvance/gba_metroidzeromission.jpg"
+import pokemonRuby from "../assets/images/Nintendo/gameboyadvance/gba_pokemonruby.jpg"
+import pokemonSapphire from "../assets/images/Nintendo/gameboyadvance/gba_pokemonsapphire.jpg"
+import sonicAdvance2 from "../assets/images/Nintendo/gameboyadvance/gba_sonicadvance2.jpg"
 
 export const GAMEBOYADVANCE = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: 'Donkey Kong Country',
+        image: donkeyKongCountry,
+        genre: 'Platformer',
+        discription: 'Remake of the Super Nintendo Classic with boss diversity and in-game cutscenes.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'Donkey Kong Country 3',
+        image: donkeyKongCountry3,
+        genre: 'Platformer',
+        discription: 'Remake of Dixie Kong\'s Double Trouble! including a new world and a new soundtrack.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'      
+        name: 'Kingdom Hearts: Chain of Memories',
+        image: kingdomHeartsChainOfMemories,
+        genre: 'Action-Role Playing',
+        discription: 'After the events of Kingdom Hearts; Sora, Donald and Goofy navigate Castle Oblivion as they encount members of Organization XIII.'      
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'        
+        name: 'The Legend of Zelda',
+        image: legendOfZelda,
+        genre: 'Action-Adventure',
+        discription: 'Port of the NES classic now for the GameBoy Advance.'        
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'The Legend of Zelda: A Link to the Past and Four Swords',
+        image: zeldaLinkToThePast,
+        genre: 'Action-Adventure',
+        discription: 'Port of the SNES classic also including the Zelda\'s series first multiplayer game.'        
     },
     {
         id: 5,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'The Legend of Zelda: Minish Cap',
+        image: zeldaMinishCap,
+        genre: 'Action-Adventure',
+        discription: 'Link team up with a talk hat, name Ezlo, to save Zelda after being turn to stone by Vaati.'        
     },
     {
         id: 6,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Mario & Luigi: Superstar Saga',
+        image: marioAndLuigiSuperStarSaga,
+        genre: 'Role-Playing Game',
+        discription: 'Mario & Luigi travles to the Beanbean Kingdom to save Princess Peach\'s voice from Cackletta.'        
     },
     {
         id: 7,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Metroid: Zero Mission',
+        image: metroidZeroMission,
+        genre: 'Action-Adventure',
+        discription: 'Remake of the original Metroid, Samus Aran is off to Zebes to face off against Mother Brain.'        
     },
     {
         id: 8,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Pokemon Ruby',
+        image: pokemonRuby,
+        genre: 'Role-Playing Game',
+        discription: 'Generation Three were you explore the Hoenn region catching and battling new pokemon.'        
     },
     {
         id: 9,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Pokemon Sapphire',
+        image: pokemonSapphire,
+        genre: 'Role-Playing Game',
+        discription: 'Generation Three were you explore the Hoenn region catching and battling new pokemon.'        
     },
     {
         id: 10,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Sonic Advance 2',
+        image: sonicAdvance2,
+        genre: 'Platformer',
+        discription: 'Sonic returns to face off Dr. Eggman to save his friends and retake the Chaos Emeralds.'        
     }
 ]
--- src/app/shared/PLAYSTATIONPORTABLEGAMES.js
+++ src/app/shared/PLAYSTATIONPORTABLEGAMES.js
@@ -1,60 +1,75 @@
-import nintendoLogo from "../assets/images/Microsoft/xbox360/Xbox360.png";
+import capcomClassicCollectionRemixed from "../assets/images/Sony/psp/psp_capcomclassicscollectionremixed.jpg"
+import daxter from "../assets/images/Sony/psp/psp_daxter.jpg"
+import finalFantasyI from "../assets/images/Sony/psp/psp_finalfantasy.jpg"
+import godOfWarChainsOfOlympus from "../assets/images/Sony/psp/psp_godofwarchainsofolympuscover.jpg"
+import grandTheftAutoLibertyCityStories from "../assets/images/Sony/psp/psp_grandtheftautolibertycitystories.jpg"
+import kingdomHeartsBirthBySleep from "../assets/images/Sony/psp/psp_kingdomheartsbirthbysleep.jpg"
+import metalGearSolidPortableOpsPlus from "../assets/images/Sony/psp/psp_metalgearsolidportableopsplus.jpg"
+import ratchetAndClankSizeMatters from "../assets/images/Sony/psp/psp_ratchetandclanksizematterscover.jpg"
+import untoldLegends from "../assets/images/Sony/psp/psp_untoldlegends.jpg"
 
 export const PLAYSTATIONPORTABLE = [
     {
         id: 0,
-        name: 'GameOne',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'aaa'
+        name: 'Capcom CLassics Collection: Remixed',
+        image: capcomClassicCollectionRemixed,
+        genre: 'Arcade',
+        discription: 'Collection of 20 Capcom games on the PSP.'
     },
     {
         id: 1,
-        name: 'GameTwo',
-        image: nintendoLogo,
-        genre: 'shooter',
-        discription: 'bbb'
+        name: 'Daxter',
+        image: daxter,
+        genre: 'Platformer',
+        discription: 'Prequel to Jak 2, Daxter worked as a bug exterminator to kill time.'
     },
     {
         id: 2,
-        name: 'GameThree',
-        image: nintendoLogo,
-        genre: 'rpg',
-        discription: 'ccc'      
+        name: 'Final Fantasy I',
+        image: finalFantasyI,
+        genre: 'Role-Playing Game',
+        discription: 'Remake for the PSP, this Final Fantasy contians higher-resolution 2D graphics, remixed sountrack and bonus dungeons.'      
     },
     {
         id: 3,
-        name: 'GameFour',
-        image: nintendoLogo,
-        genre: 'puzzle',
-        discription: 'ddd'        
+        name: 'God of War: Chains of Olympus',
+        image: godOfWarChainsOfOlympus,
+        genre: 'Action-Adventure',
+        discription: 'Before the events of the first God of War, Kratos witness the sun falling from the sky.'        
     },
     {
         id: 4,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Grand Theft Auto: Liberty City Stories',
+        image: grandTheftAutoLibertyCityStories,
+        genre: 'Action-Adventure',
+        discription: 'Leone mobster Toni return to Liberty City to continue his work for Don Salvatore Leone.'        
     },
     {
         id: 5,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Kingdom Hearts Birth By Sleep',
+        image: kingdomHeartsBirthBySleep,
+        genre: 'Action Role-Playing',
+        discription: 'Prequel to Kingdom Hearts, play as Terra, Ventus or Aqua as their stories lead to there destiny.'
     },
     {
         id: 6,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Metal Gear Soild: Portable Ops Plus',
+        image: metalGearSolidPortableOpsPlus,
+        genre: 'Stealth Action',
+        discription: 'A stand-alone expansion of Portable Ops, which includes Infinity Missions mode.'        
     },
     {
         id: 7,
-        name: 'GameFive',
-        image: nintendoLogo,
-        genre: 'platformer',
-        discription: 'eee'        
+        name: 'Ratchet and Clank: Size Matters',
+        image: ratchetAndClankSizeMatters,
+        genre: 'Third Person Shooter',
+        discription: 'A spin-off for the PSP, Ratchet and Clank are off to save a kidnapped girl.'        
     },
+    {
+        id: 8,
+        name: 'Untold Legends: Brotherhood of the Blade',
+        image: untoldLegends,
+        genre: 'Action Role-Playing',
+        discription: 'Travel outside the city of Aven and fight the forces of evil.'        
+    }
 ]
