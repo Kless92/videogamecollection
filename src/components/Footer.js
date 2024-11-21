fb714042b07f49c6c0144a64f7ad2f7dc1be8c18
Spencer Klessens <klessens92@gmail.com>
2024-11-21 12:55:58 -0600

    return to previous page button made in Footer and borders for discription page is finished.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,24 @@
 import { Navbar, NavbarBrand, Button, NavItem, Nav, Row } from "reactstrap";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+
 
 const Footer = () => {
+    let history = useNavigate();
+
     return(
         <Navbar dark color='primary' className="snapFooter">
-            <NavbarBrand>
-                <h1>TEST</h1>
-            </NavbarBrand>
-            <Nav navbar>
+
+            <Nav style={{margin:'auto'}} navbar>
                 <Row> {/*Home Page*/}
                     <NavItem>
                         <NavLink className='nav-link' to='/'>
-                            <i className='fa fa-home fa-lg' /> Home
+                            <i className='fa fa-home' style={{fontSize:'30px'}}> H O M E </i>
                         </NavLink>
                     </NavItem>
                 </Row>
             </Nav>
-            <Button className='butt'>
-
+            <Button className='butt' onClick={() => history(-1)}>
+                <i >Return</i>
             </Button>
         </Navbar>
     )
--- src/features/Display/Discription.js
+++ src/features/Display/Discription.js
@@ -8,21 +8,21 @@ const Discription = ({ items }) => {
     const { name, image, genre, discription } = items
 
     return (
-        <div className="test">
-            <Row >
+        <Table style={{marginTop:'15px'}}>
+            <Row>
                 <Col sm='4' />
-                <Col sm='2'>
+                <Col sm='2' className="test" style={{ borderRight: 'white' }}>
                     <img src={image} alt={name} className="shrink" />
                 </Col>
-                <Col sm='2' >
+                <Col sm='2' className="test" style={{ borderLeft: 'white' }}>
                     <br />
-                    <a style={{fontSize: 30, display: 'flex', justifyContent: 'left'}}>{name}</a>
-                    <a style={{fontSize: 20, display: 'flex', justifyContent: 'left'}}>{genre}</a>
-                    <br/>
-                    {discription}                 
+                    <a style={{ fontSize: 30, display: 'flex', justifyContent: 'left' }}>{name}</a>
+                    <a style={{ fontSize: 20, display: 'flex', justifyContent: 'left' }}>{genre}</a>
+                    <br />
+                    {discription}
                 </Col>
             </Row>
-        </div>
+        </Table>
     )
 };
 
