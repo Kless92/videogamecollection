acdb617a0f0d83fd0fdd7c86697f3f1ba94dea0c
Spencer Klessens <klessens92@gmail.com>
2024-12-04 13:14:48 -0600

    new images added and home page is edited with thoes images added.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,10 +1,23 @@
-import { Container } from "reactstrap";
-
+import { Container, Row, Col } from "reactstrap";
+import collectionOne from '../app/assets/images/gamesAndToys.jpeg';
+import hdSetup from '../app/assets/images/hdSetup.jpeg';
+import retroSetup from '../app/assets/images/retroSetup.jpeg';
 const HomePage = () => {
     return (
-        <Container> 
+        <div style={{margin: '25px'}}> 
             <h1>Welcome to my video game collection home page</h1>
-        </Container>
+            <Row >
+                <Col>
+                    <img src={retroSetup} alt={"ERROR"} className="test" style={{height: "400px"}}/>
+                </Col>
+                <Col>
+                    <img src={hdSetup} alt={"ERROR"} className="test" style={{height: "400px"}}/>
+                </Col>
+                <Col>
+                    <img src={collectionOne} alt={"ERROR"} className="test" style={{height: "400px"}}/>
+                </Col>
+            </Row>
+        </div>
     )
 };
 
